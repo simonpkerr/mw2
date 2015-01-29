541c403081657b7513cfb05d8535d998abd850c3
Simon Kerr <simon.p.kerr@gmail.com>
2015-01-29 18:18:33 +0000

    updated mw controller spec.

--- web/bundles/Sk/test/spec/controllers/app.memoryWallSpec.js
+++ web/bundles/Sk/test/spec/controllers/app.memoryWallSpec.js
@@ -1,45 +1,90 @@
 'use strict';
+//look here http://projectpoppycock.com/2014/03/05/mocking-resource-and-promises-in-angularjs-unit-tests/
 
 describe('MemoryWall controller', function () {
+    var vm,
+            $q,
+            $rootScope,
+            $scope,
+            $httpBackend,
+            mockMemoryWallPrepService,
+            mockMemoryWallService,
+            mockYouTubeService,
+            wallFixtures =
+            {
+                wallData: {
+                    metaData: {
+                        decade: "1940s",
+                        pageNumber: 1
+                    },
+                    providerData: [{
+                            provider: "amazon",
+                            id: "B004I8WHCO",
+                            title: "Fantasia - [DVD] [1940]",
+                            image: "FwAurdDHL._SL160_.jpg",
+                            url: "http://www.amazon.co.uk",
+                            price: ""
+                        }]
+                }
+            };
 
-  // load the controller's module
-  beforeEach(module('mwApp'));
-  beforeEach(module('mwApp.memoryWall'));
-    
-  var ctrl, scope, $httpBackend,
-  wallFixtures = 
-    {
-        wallData:{ 
-            metaData:{ 
-                decade:"1940s",
-                pageNumber:1
-            },
-            providerData:[{
-                provider:"amazon",
-                id:"B004I8WHCO",
-                title:"Fantasia - [DVD] [1940]",
-                image:"FwAurdDHL._SL160_.jpg",
-                url:"http://www.amazon.co.uk",
-                price:""
-            }]
-        }
-    };
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/web/app_dev.php/api/memorywall')
-        .respond(wallFixtures);
-    
-    scope = $rootScope.$new();
-    ctrl = $controller('MemoryWall', {
-      $scope: scope
+    // load the controller's module
+    beforeEach(module('mwApp'));
+    beforeEach(module('mwApp.memoryWall'));
+
+
+//  module(function($provide) {
+//     $provide.factory('memoryWallService', function(){
+//         this.memoryWall = jasmine.createSpy('memoryWall').andCallFake(function(){
+//             return 
+//         });
+//     }); 
+//  });
+//  
+
+
+//    memoryWallPrepService = {
+//        memoryWall: memoryWallService.memoryWall(),
+//        getYouTubePlayer: youTubeService.getPlayer
+//    };
+//        
+
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function (_$q_, _$rootScope_, _$controller_) {
+//    $httpBackend = _$httpBackend_;
+//    $httpBackend.expectGET('/web/app_dev.php/api/memorywall')
+//        .respond(wallFixtures);
+        $q = _$q_;
+        $rootScope = _$rootScope_;
+    }));
+
+    beforeEach(inject(function ($controller) {
+        $scope = $rootScope.$new();
+        mockMemoryWallService = {
+            query: function() {
+                queryDeferred = $q.defer();
+                return { 
+                    $promise: queryDeferred.promise 
+                };
+            }
+        };
+        spyOn(mockMemoryWallService, 'query').andCallThrough();
+        
+        mockMemoryWallPrepService = {
+            memoryWall: mockMemoryWallService.memoryWall()
+        };
+        
+        vm = $controller('MemoryWall', {
+            '$scope': $scope,
+            'memoryWallPrepService': mockMemoryWallPrepService
+        });
+    }));
+
+    it('should load data from the api on load', function () {
+        expect(vm.wallData.hasOwnProperty("providerData")).toBe(false);
+        $httpBackend.flush();
+        expect(vm.wallData.hasOwnProperty("providerData")).toEqual(true);
     });
-  }));
-
-  it('should load data from the api on load', function () {
-    expect(scope.wallData).toBe({});
-    $httpBackend.flush();
-    expect(scope.wallData).toEqual(wallFixtures);
-  });
 });
